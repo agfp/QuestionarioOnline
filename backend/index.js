2ff86682b3f6a325aa107903a4ea4d0e674c0501
Alexandre Giuseppe de Fuccio Pereira <alexandre@alextech.com.br>
2017-07-20 17:49:19 -0300

    Backend: is working

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,5 @@
+/* eslint-env node */
+
 var express = require('express');
 var app = express();
 
@@ -9,12 +11,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
-  response.render('pages/index');
-});
+// app.get('/', function(request, response) {
+//     response.render('pages/index');
+// });
 
 app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
-
-
