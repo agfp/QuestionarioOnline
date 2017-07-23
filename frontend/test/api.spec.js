ebf53a1b82b1cc0f4e1a7ff7da953f6bb994f3d3
Alexandre Giuseppe de Fuccio Pereira <alexandre@alextech.com.br>
2017-07-23 05:15:29 -0300

    Frontend: configuring tests

---
+++ frontend/test/api.spec.js
@@ -0,0 +1,10 @@
+// const User = require('./User');
+const expect = require('chai').expect;
+
+describe('User module', () => {
+    describe('"up"', () => {
+        it('should export a function', () => {
+            expect(true).to.be.a('boolean');
+        });
+    });
+});
