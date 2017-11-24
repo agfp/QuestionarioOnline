07c42e0dbf994f18f11e88c7844f53b8f90829b6
Alexandre Giuseppe de Fuccio Pereira <alexandregiuseppe@gmail.com>
2017-11-24 10:00:10 -0200

    tcle

--- frontend/src/components/consents/Generic.vue
+++ frontend/src/components/consents/Generic.vue
@@ -1,10 +1,48 @@
 <template>
 <div class="texto">
     <p>
-        bla bla bla
+        Este é um convite para você participar da pesquisa, "Qualidade de vida, condições de trabalho de servidores da UFMG", que
+        é coordenada pela Profa. <i>Livia de Oliveira Borges</i> do Departamento de Psicologia da UFMG (Belo Horizonte).
+        Você participa apenas se você quiser e que você poderá desistir a qualquer momento, retirando seu consentimento,
+        sem que isso lhe traga nenhum prejuízo ou penalidade.
     </p>
-
-
+    <p>
+        Essa pesquisa tem como objetivo explorar a percepção das condições de trabalho e suas relações com a qualidade de vida no
+        trabalho (QVT) para servidores (funcionários técnico-administrativos e docentes). Caso decida aceitar o convite,
+        você será submetido(a) ao(s) seguinte(s) procedimentos: responder a questionários. O arquivamento de seu consentimento
+        e de suas respostas ocorrerá em banco de dados digitais distintos. Não haverá a possibilidade de vincular o consentimento
+        a um questionário em específico. Assim, você responderá sem precisar dizer ou anotar seu nome e será possível manter
+        o seu anonimato.
+    </p>
+    <p>
+        Os riscos envolvidos com sua participação consistem na exposição de suas opiniões; riscos que está sendo minimizado pela
+        providência de não o identificar, ou seja, de não anotar o seu nome. As perguntas são acerca do ambiente em que você
+        trabalha e como você se sente e reage em relação a ele. Suas respostas serão guardadas de forma confidencial e a
+        divulgação dos resultados será feita de forma a não identificar os participantes, focalizando o seu conteúdo geral
+        e nos resultados estatísticos.
+    </p>
+    <p>
+        Você não terá benefícios pessoais diretos ao participar da pesquisa, mas os resultados poderão servir de critérios e/ou subsídios
+        que fundamentem o planejamento de políticas e ações da gestão de pessoas na UFMG. Assim, você poderá ser beneficiado
+        indiretamente.
+    </p>
+    <p>
+        Não estamos prevendo que você venha a ter quaisquer despesas ou danos em decorrência de sua participação, mas se despesas
+        ou danos vierem a ocorrer você será ressarcido ou indenizado conforme o caso.
+    </p>
+    <p>
+        Você pode ficar com uma cópia deste Termo, salvando no ícone específico disponível. Toda e qualquer dúvida que você tiver
+        a respeito desta pesquisa, poderá perguntar diretamente para a Profa. <i>Livia de Oliveira Borges</i>, em
+        Belo Horizonte (endereço e telefone especificado ao final). Dúvidas a respeito da ética dessa pesquisa também poderão
+        ser questionadas ao Comitê de Ética em Pesquisa da UFMG conforme endereço também especificado ao final.
+    </p>
+    <div class="pure-g">
+        <div class="pure-u-1-1 signature">
+            <p>
+                <span class="bold">Profa. Dra. Livia de Oliveira Borges</span><br /> Coordenadora
+            </p>
+        </div>
+    </div>
     <p>
         <span class="bold">Endereço:</span> Departamento de Psicologia, da Faculdade de Filosofia e Ciências Humanas (FAFICH)
         da Universidade Federal de Minas Gerais (UFMG), Sala 4100. Campus Pampulha. Av. Antônio Carlos, 6627. CEP 31270-901.
