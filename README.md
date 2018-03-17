8aaf63c236d1f30e7092a08196d45bd7346480ac
Alexandre Giuseppe de Fuccio Pereira <alexandregiuseppe@gmail.com>
2018-03-17 02:31:26 -0300

    README.md

---
+++ README.md
@@ -0,0 +1,7 @@
+# Online Survey System
+
+This is an online survey system about work conditions on a governmental institution.
+
+Both frontend and backend are in JavaScript/ES6. Frontend in VueJS and backend in Express.
+
+Credentials were not sanitized because they are useless at the time of public publishing.
--- build.sh
+++ build.sh
@@ -5,11 +5,10 @@ BUILD_NAME="ufmg0"
 cd frontend
 npm run build
 cd ..
-rsync -av frontend/dist/static/css backend/public/static --exclude *.map 
-rsync -av frontend/dist/static/js backend/public/static --exclude *.map 
+rsync -av frontend/dist/static/css backend/public/static --exclude *.map
+rsync -av frontend/dist/static/js backend/public/static --exclude *.map
 
 cp -v frontend/dist/index.html backend/views/questionnaires/$BUILD_NAME.ejs
 
 cd backend/views/questionnaires
 sed -i -- 's/{"key":"key","token":"token"}/<%- parameters %>/g' $BUILD_NAME.ejs
-#cd ../..
